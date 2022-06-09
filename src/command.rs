45458868c1458dfe0a99c5002c19cd6a01e15cc3
Techseeker-404 <anandml4u@gmail.com>
2022-06-09 12:19:28 +0530

    Initial commit

---
+++ src/command.rs
@@ -0,0 +1,71 @@
+
+/// C Code COMMANDS in we need to create alternate RUST set
+
+
+//  #define ST7796S_NOP            0x00
+//  #define ST7796S_SWRESET        0x01
+//  
+//  #define ST7796S_RDDID          0x04
+//  #define ST7796S_RDDST          0x09
+//  #define ST7796S_RDMODE         0x0A
+//  #define ST7796S_RDMADCTL       0x0B
+//  #define ST7796S_RDPIXFMT       0x0C
+//  #define ST7796S_RDIMGFMT       0x0D
+//  #define ST7796S_RDSELFDIAG     0x0F
+//  
+//  #define ST7796S_SLPIN          0x10
+//  #define ST7796S_SLPOUT         0x11
+//  #define ST7796S_PTLON          0x12
+//  #define ST7796S_NORON          0x13
+//  
+//  #define ST7796S_INVOFF         0x20
+//  #define ST7796S_INVON          0x21
+//  //#define ST7796S_GAMMASET       0x26
+//  #define ST7796S_DISPOFF        0x28
+//  #define ST7796S_DISPON         0x29
+//  
+//  #define ST7796S_CASET          0x2A
+//  #define ST7796S_PASET          0x2B
+//  #define ST7796S_RAMWR          0x2C
+//  #define ST7796S_RAMRD          0x2E
+//  
+//  #define ST7796S_PTLAR          0x30
+//  #define ST7796S_VSCRDEF        0x33
+//  #define ST7796S_MADCTL         0x36
+//  #define ST7796S_VSCRSADD       0x37     /* Vertical Scrolling Start Address */
+//  #define ST7796S_PIXFMT         0x3A     /* COLMOD: Pixel Format Set */
+//  
+//  #define ST7796S_RGB_INTERFACE  0xB0     /* RGB Interface Signal Control */
+//  #define ST7796S_FRMCTR1        0xB1
+//  #define ST7796S_FRMCTR2        0xB2
+//  #define ST7796S_FRMCTR3        0xB3
+//  #define ST7796S_INVCTR         0xB4
+//  #define ST7796S_DFUNCTR        0xB6     /* Display Function Control */
+//  
+//  #define ST7796S_PWCTR1         0xC0
+//  #define ST7796S_PWCTR2         0xC1
+//  #define ST7796S_PWCTR3         0xC2
+//  #define ST7796S_PWCTR4         0xC3
+//  #define ST7796S_PWCTR5         0xC4
+//  #define ST7796S_VMCTR1         0xC5
+//  
+//  #define ST7796S_RDID1          0xDA
+//  #define ST7796S_RDID2          0xDB
+//  #define ST7796S_RDID3          0xDC
+//  #define ST7796S_RDID4          0xDD
+//  
+//  #define ST7796S_GMCTRP1        0xE0
+//  #define ST7796S_GMCTRN1        0xE1
+//  #define ST7796S_DGCTR1         0xE2
+//  #define ST7796S_DGCTR2         0xE3
+//  
+//  //-----------------------------------------------------------------------------
+//  #define ST7796S_MAD_RGB        0x08
+//  #define ST7796S_MAD_BGR        0x00
+//  
+//  #define ST7796S_MAD_VERTICAL   0x20
+//  #define ST7796S_MAD_X_LEFT     0x00
+//  #define ST7796S_MAD_X_RIGHT    0x40
+//  #define ST7796S_MAD_Y_UP       0x80
+//  #define ST7796S_MAD_Y_DOWN     0x00
+//  
